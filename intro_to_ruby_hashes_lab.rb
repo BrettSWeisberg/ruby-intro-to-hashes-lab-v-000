ea5e0936e8848c5ccc68385b0a80ea16265c5c15
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-15 00:07:43 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly
 		:railroads => {:pieces => 4,:names => {}, :rent_in_dollars => {}}
 	}
 end
-
+binding.pry
 def monopoly_with_second_tier
 	monopoly = {
 		:railroads => {:pieces => 4,:names => {}, :rent_in_dollars => {}},
